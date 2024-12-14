cbfa6c7300b8b8a9b6a173f6777ff85b05193978
N1CKOLAY <nickolay.bludov@soteranalytics.com>
2024-12-14 22:56:14 +0100

    feat: basic handlers. watch command handler

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,60 @@
-import type { Handler } from '@netlify/functions';
+import type { Handler } from '@netlify/functions'
+import { parse } from 'querystring'
+import { slackApi, verifySlackRequst } from './util/slack'
 
-export const handler: Handler = async () => {
-	// TODO validate the Slack request
+async function handleSlashCommandf(payload: SlackSlashCommandPayload) {
+  const { command, channel_id, text } = payload
+  switch (command) {
+    case '/watch':
+      const movie = await (
+        await fetch(
+          `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&t=${text}`
+        )
+      ).json()
+      let message: string
+      if (movie.Response === 'False') {
+        message = 'There is no such a movie found'
+      } else {
+        message = `${movie.Title} - ${movie.Year}
+${movie.Plot}`
+      }
+      const res = await slackApi('chat.postMessage', {
+        channel: channel_id,
+        text: message,
+      })
+      if (!res.ok) {
+        console.log('ERROR', res)
+      }
+      break
+    default:
+      return {
+        statusCode: 200,
+        body: `Unknown command: ${command}`,
+      }
+  }
+  return {
+    statusCode: 200,
+    body: '',
+  }
+}
 
-	// TODO handle slash commands
+export const handler: Handler = async (event) => {
+  const valid = verifySlackRequst(event)
+  if (!valid) {
+    console.log('Invalid/unathorized request')
+    return {
+      statusCode: 401,
+      body: 'Invalid/unathorized request',
+    }
+  }
 
-	// TODO handle interactivity (e.g. context commands, modals)
+  const body = parse(event.body ?? '') as SlackPayload
+  if (body.command) return handleSlashCommandf(body as SlackSlashCommandPayload)
 
-	return {
-		statusCode: 200,
-		body: 'TODO: handle Slack commands and interactivity',
-	};
-};
+  // TODO handle interactivity (e.g. context commands, modals)
+
+  return {
+    statusCode: 200,
+    body: 'TODO: handle Slack commands and interactivity',
+  }
+}
--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,1 +1,123 @@
-// TODO create Slack utilities
+import type { HandlerEvent } from '@netlify/functions'
+import { createHmac } from 'node:crypto'
+
+export async function slackApi(
+  endpoint: SlackApiEndpoint,
+  body: SlackApiRequestBody
+) {
+  return fetch(`https://slack.com/api/${endpoint}`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN}`,
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify(body),
+  }).then((res) => res.json())
+}
+
+export function verifySlackRequst(req: HandlerEvent) {
+  const secret = process.env.SLACK_SIGNIN_SECRET!
+  const signature = req.headers['x-slack-signature']
+  const timestamp = Number(req.headers['x-slack-request-timestamp'])
+  const now = Math.floor(Date.now() / 1000)
+
+  if (Math.abs(now - timestamp) > 300) return false
+
+  const hash = createHmac('sha256', secret)
+    .update(`v0:${timestamp}:${req.body}`)
+    .digest('hex')
+
+  return `v0=${hash}` === signature
+}
+
+export const blocks = {
+  section: ({ text }: SectionBlockArgs): SlackBlockSection => ({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text,
+    },
+  }),
+  input: ({
+    id,
+    label,
+    placeholder,
+    initial_value = '',
+    hint = '',
+  }: InputBlockArgs): SlackBlockInput => ({
+    type: 'input',
+    block_id: `${id}_block`,
+    label: {
+      type: 'plain_text',
+      text: label,
+    },
+    element: {
+      action_id: id,
+      type: 'plaint_text_input',
+      placeholder: {
+        type: 'plain_text',
+        text: placeholder,
+      },
+      initial_value,
+    },
+    hint: {
+      type: 'plain_text',
+      text: hint,
+    },
+  }),
+  select: ({
+    id,
+    label,
+    placeholder,
+    options,
+  }: SelectBlockArgs): SlackBlockInput => ({
+    block_id: `${id}_block`,
+    type: 'input',
+    label: {
+      type: 'plain_text',
+      text: label,
+    },
+    element: {
+      action_id: id,
+      type: 'static_select',
+      placeholder: {
+        type: 'plain_text',
+        text: placeholder,
+        emoji: true,
+      },
+      options: options.map(({ label, value }) => ({
+        text: {
+          type: 'plain_text',
+          text: label,
+          emoji: true,
+        },
+        value,
+      })),
+    },
+  }),
+}
+
+export function modal({
+  trigger_id,
+  id,
+  title,
+  submit_text,
+  blocks,
+}: ModalArgs) {
+  return {
+    trigger_id,
+    view: {
+      type: 'modal',
+      callback_id: id,
+      title: {
+        type: 'plain_text',
+        text: title,
+      },
+      submit: {
+        type: 'plain_text',
+        text: submit_text,
+      },
+      blocks,
+    },
+  }
+}
